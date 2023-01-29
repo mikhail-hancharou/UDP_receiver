decdc280a684692255aa17da1baac3c9d92d9f6e
Mikhail Hancharou <engeneer2003@mail.ru>
2023-01-29 17:32:38 +0300

    Final release

--- UDP_receiver.c
+++ UDP_receiver.c
@@ -1,20 +1,20 @@
-﻿// UPD_receiver.cpp : Defines the entry point for the application.
-//
-
-#include "UDP_receiver.h"
+﻿#include "UDP_receiver.h"
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include "errno.h"
 #include "sqlite3.h"
 #include <netinet/in.h>
-#include <arpa/inet.h> //
+#include <arpa/inet.h>
 #include <regex.h>
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
+#include <pwd.h>
+#include <unistd.h>
+//#define DEBUG
 
-
+#ifdef  DEBUG
 bool SendEcho(int sock, char* IP, int PORT)
 {
 	struct sockaddr_in sendAddr;
@@ -32,12 +32,14 @@ bool SendEcho(int sock, char* IP, int PORT)
 	sendto(sock, buf, sizeof(buf), 0, (struct sockaddr*)&sendAddr, sizeof(sendAddr));
 	return true;
 }
+#endif //  DEBUG
 
 void RecvEcho(int sock, sqlite3* db, sqlite3_stmt* stmt)
 {
 	struct sockaddr_in recvAddr;
 	int size = sizeof(recvAddr);
 	char buf[1024];
+	puts("Waiting for message...");
 	int bytes_read = recvfrom(sock, buf, 1024, 0, (struct sockaddr*)&recvAddr, &size);
 	time_t t = time(NULL);
 	if (bytes_read < 0)
@@ -63,45 +65,63 @@ void RecvEcho(int sock, sqlite3* db, sqlite3_stmt* stmt)
 	fflush(stdout);
 }
 
+int convertToInt(const char *a)
+{
+	int i = 0;
+	int num = 0;
+	int size = strlen(a);
+	for (i; i < size; i++)
+	{
+		num = (a[i] - '0') + (num * 10);
+	}
+	return num;
+}
 
 int main()
 {
 	sqlite3* db;
 	sqlite3_stmt* stmt;
-	int term_res = sqlite3_open("test.db", &db);
+	struct passwd* pw = getpwuid(getuid());
+	const char* homedir = pw->pw_dir;
+	char dir[64];
+	sprintf(dir, "%s/test.db", homedir);
+	int term_res = sqlite3_open(dir, &db);//test.db
 	if (term_res != SQLITE_OK)//":memory:"
 	{
 		printf("Cannot open db connection: %s\n", sqlite3_errmsg(db));
 		return 1;
 	}
+	else
+	{
+		printf("Successfully opened connection with db\n");
+	}
 	//./home/adduser/.vs/UDP_receiver/out/build/linux-debug/test.db
-	//char* sql = "CREATE TABLE Packets(Id INTEGER PRIMARY KEY, Ip TEXT, Size INT, TimeIn DATETIME, TimeWrite DATETIME DEFAULT CURRENT_TIMESTAMP);";// InTime TIMESTAMP, WriteTime TIMESTAMP
-	//char* sql = "INSERT INTO Packets(Ip) VALUES ('12345678');";
+	char* sql = "CREATE TABLE IF NOT EXISTS Packets(Id INTEGER PRIMARY KEY, Ip TEXT, Size INT, TimeIn DATETIME, TimeWrite DATETIME DEFAULT CURRENT_TIMESTAMP);";
 	char* err_msg;
-	//term_res = sqlite3_exec(db, sql, 0, 0, &err_msg);
-	//if (term_res != SQLITE_OK)//":memory:"
-	//{
-	//	printf("SQL error: %s\n", err_msg);
-	//	sqlite3_free(err_msg);
-	//	sqlite3_close(db);
-	//	return 1;
-	//}
+	term_res = sqlite3_exec(db, sql, 0, 0, &err_msg);
+	if (term_res != SQLITE_OK)
+	{
+		printf("SQL error: %s\n", err_msg);
+		sqlite3_free(err_msg);
+		sqlite3_close(db);
+		return 1;
+	}
 
 	regex_t reg;
-	int compRes = regcomp(&reg, IP_REGEX, REG_EXTENDED);
 	char IP[16];
 	int PORT;
 	while (1)
 	{
 		printf("Type IP or q to exit: ");
 		scanf("%s", IP);
-		//fgets(IP, sizeof(IP), stdin);
+		sscanf(IP, "%s", IP);
+		int compRes = regcomp(&reg, IP_REGEX, REG_EXTENDED);
 		int regRes = regexec(&reg, IP, 0, NULL, 0);
 		if (regRes == 0)
 		{
 			printf("IP confirmed [%s]\n", IP);
 		}
-		else //if (regRes == REG_NOMATCH)
+		else
 		{
 			if (IP[0] == 'q')
 			{
@@ -112,16 +132,23 @@ int main()
 		}
 
 		char port[6];
-		printf("Type PORT: ");
-		scanf(" %d", &PORT);
-		if (PORT >= 1 && PORT <= 65535)
+		regcomp(&reg, PORT_REGEX, REG_EXTENDED);
+		while (1)
 		{
-			printf("Port confirmed [%d]\n", PORT);
-		}
-		else
-		{
-			printf("Incorrect PORT. Try again\n");
-			continue;
+			printf("Type PORT: ");
+			scanf("%s", port);
+			regRes = regexec(&reg, port, 0, NULL, 0);
+			PORT = convertToInt(port);
+			if (regRes == 0 && PORT >= 1024 && PORT <= 65535)
+			{
+				printf("Port confirmed [%d]\n", PORT);
+				break;
+			}
+			else
+			{
+				puts("Incorrect PORT. Try again");
+				continue;
+			}
 		}
 
 		int sock;
@@ -129,10 +156,14 @@ int main()
 		sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 		if (sock < 0)
 		{
-			printf("Failed to create socket. Error %d", errno);
-			fflush(stdout);
+			printf("Failed to create socket. Error %d\n", errno);
 			return 1;
 		}
+		else
+		{
+			printf("Socket successfully created\n");
+		}
+		fflush(stdout);
 
 		char buf[1024];
 		int bytes_read;
@@ -142,11 +173,21 @@ int main()
 		addr_in.sin_addr.s_addr = inet_addr(IP);//htons(INADDR_ANY)
 		if (bind(sock, (struct sockaddr*)&addr_in, sizeof(addr_in)) < 0)
 		{
-			printf("Failed to bind socket. Error %d", errno);
-			fflush(stdout);
+			printf("Failed to bind socket. Error %d\n", errno);
 			return 1;
 		}
+		else
+		{
+			printf("Successfully binded\n");
+		}
+		fflush(stdout);
 
+		while (1)
+		{
+			RecvEcho(sock, db, stmt);
+		}
+
+		#ifdef DEBUG
 		while (1)
 		{
 			if (!SendEcho(sock, IP, PORT))
@@ -155,6 +196,7 @@ int main()
 			}
 			RecvEcho(sock, db, stmt);
 		}
+		#endif // DEBUG
 	}
 
 	sqlite3_close(db);
--- UDP_receiver.h
+++ UDP_receiver.h
@@ -1,6 +1,5 @@
-﻿// UDP_receiver.h : Include file for standard system include files,
-// or project specific include files.
-#define IP_REGEX "^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])\.){3}((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9]))$"
+﻿#define IP_REGEX "^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])\\.){3}((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9]))$"
+#define PORT_REGEX "^([1-9][0-9]?[0-9]?[0-9]?[0-9]?)$"
 #define SQL_INSERT_BASE "INSERT INTO Packets(Ip, Size, TimeIn) VALUES ("
 
 #pragma once
